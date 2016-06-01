a33b2c243bd101137b2661df401df74d996c0d09
dennisGER13 <dw-161380@hs-weingarten.de>
2016-06-01 11:19:40 +0200

    Twitter Response edited

--- Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/api/response/TwitterResponse.java
+++ Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/api/response/TwitterResponse.java
@@ -2,7 +2,50 @@ package advswen.team5.travelbutler.api.response;
 
 public class TwitterResponse implements IAPIResponse{
 
+	private String user;
+	private String tweet;
+	private boolean missing = false;
 	
 	
+	public TwitterResponse(String user, String tweet, boolean missing) {
+		super();
+		this.user = user;
+		this.tweet = tweet;
+		
+		if (tweet == null || tweet.length() > 1) {
+			this.missing = true;
+		}
+
+	}
+
+
+	public String getUser() {
+		return user;
+	}
+
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+
+	public String getTweet() {
+		return tweet;
+	}
+
+
+	public void setTweet(String tweet) {
+		this.tweet = tweet;
+	}
+
+
+	public boolean isMissing() {
+		return missing;
+	}
+
+
+	public void setMissing(boolean missing) {
+		this.missing = missing;
+	}
 	
 }
