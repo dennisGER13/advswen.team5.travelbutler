54923518f2e853ac33a9fd3150c24b0444536ab4
Andreas Tauscher <andreas.tauscher@sap.com>
2016-06-01 10:40:06 +0200

    Added Wikipedia API

---
+++ Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/api/WikipediaContainer.java
@@ -0,0 +1,62 @@
+// *************************************************************************************
+// File:         [APIContainerFoo.java]
+// Created:      [2016/05/24 Tuesday]
+// Last Changed: $Date: 2016/05/24 18:19:00 $
+// Author:       <A HREF="mailto:[al-161385@hs-weingarten.de]">[Alexander Lohr]</A>
+//**************************************************************************************
+//Description: 	Klasse f�r die Beschaffung von Informationen durch eine API
+//				
+//**************************************************************************************
+package advswen.team5.travelbutler.api;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import advswen.team5.travelbutler.api.response.IAPIResponse;
+import advswen.team5.travelbutler.api.response.WikipediaResponse;
+
+public class WikipediaContainer implements IAPIContainer {
+
+	@Override
+	public IAPIResponse processSearch(String requestString) {
+		Gson gson = new Gson();
+		
+		WikipediaResponse response = gson.fromJson(getPageAsJson(requestString), WikipediaResponse.class);
+		System.out.println(response.getExtract());
+		return response;
+
+	}
+	
+	private JsonObject getPageAsJson(String requestString) {
+		InputStream in = null;
+		JsonObject page = null;
+		try {
+			in = new URL("https://de.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&indexpageids=1&titles="+ requestString +"&exintro=1&explaintext=1").openStream();
+			JsonParser parser = new JsonParser();
+			JsonObject o = parser.parse(IOUtils.toString(in)).getAsJsonObject().getAsJsonObject("query");
+			
+			String pageId = o.getAsJsonArray("pageids").get(0).getAsString();
+			return o.getAsJsonObject("pages").getAsJsonObject(pageId);
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+		
+		return page;
+	}
+}
--- Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/api/response/WikipediaResponse.java
+++ Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/api/response/WikipediaResponse.java
@@ -1,40 +1,54 @@
 package advswen.team5.travelbutler.api.response;
 
 public class WikipediaResponse implements IAPIResponse {
+	private String pageid;
 	private String title;
-	private String content;
+	private String extract;
 	private boolean missing = false;
-	
-	public WikipediaResponse(String title, String content) {
+
+	public WikipediaResponse(String pageid, String title, String extract) {
 		super();
+		this.pageid = pageid;
 		this.title = title;
-		this.content = content;
-		
+		this.extract = extract;
+
 		// If no content is returend the page is not existing
 		// TODO: Check for "Missing"-Attribute in response
-		if(content == null || content.length() > 1){
+		if (extract == null || extract.length() > 1) {
 			this.missing = true;
 		}
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getContent() {
-		return content;
+
+	public String getPageid() {
+		return pageid;
 	}
-	public void setContent(String content) {
-		this.content = content;
+
+	public void setPageid(String pageid) {
+		this.pageid = pageid;
 	}
+
+	public String getExtract() {
+		return extract;
+	}
+
+	public void setExtract(String extract) {
+		this.extract = extract;
+	}
+
 	public boolean isMissing() {
 		return missing;
 	}
+
 	public void setMissing(boolean missing) {
 		this.missing = missing;
 	}
-	
-	
 
 }
