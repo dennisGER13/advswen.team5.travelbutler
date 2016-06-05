44e6c1cc9e974934330f57a335167b798280d3ed
Michael Aulbach <ma-152478@hs-weingarten.de>
2016-06-05 13:35:46 +0200

    Tests for APIContainerWikipedia added

--- Travelbutler_Engine/test/advswen/team5/travelbutler/api/APIContainerWikipediaTest.java
+++ Travelbutler_Engine/test/advswen/team5/travelbutler/api/APIContainerWikipediaTest.java
@@ -2,25 +2,57 @@ package advswen.team5.travelbutler.api;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import advswen.team5.travelbutler.api.response.IAPIResponse;
 
 public class APIContainerWikipediaTest {
+	
+	private IAPIContainer iapiContainer;
+
+	@Before
+	public void setup(){
+		System.out.println("Setup");
+		iapiContainer = new APIContainerWikipedia();
+	
+	}
+	
 
 	@Test
-	public void wikipediaResponse_should_not_be_null() {
-		IAPIContainer iapiContainer = new APIContainerWikipedia();
+	public void processSearchShouldNotBeNullForValidInputString() {
+		System.out.println("Test: processSearchShouldNotBeNullForValidInputString");
 		IAPIResponse processSearch = iapiContainer.processSearch("Frankreich");
 		assertNotNull(processSearch);
 		
 	}
 	
 	@Test
-	public void isMissing_should_be_false() {
-		IAPIContainer iapiContainer = new APIContainerWikipedia();
+	public void isMissingShouldBeFalseForValidInputString() {
+		System.out.println("Test: isMissingShouldBeFalseForValidInputString");
 		IAPIResponse processSearch = iapiContainer.processSearch("Frankreich");
 		assertFalse(processSearch.isMissing());
 	}
+	
+	@Test(expected = NullPointerException.class)
+	public void processSearchShouldThrowNullPointerExceptionForNullString() {
+		System.out.println("Test: processSearchShouldThrowNullPointerExceptionForNullString");
+		IAPIResponse processSearch = iapiContainer.processSearch(null);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void processSearchShouldThrowNullPointerExceptionForBlankString() {
+		System.out.println("Test: processSearchShouldThrowNullPointerExceptionForBlankString");
+		IAPIResponse processSearch = iapiContainer.processSearch("");
+	}
+	
+	@After
+	public void cleanup() {
+		System.out.println("Cleanup");
+		iapiContainer = null;
+		
+	}
+
 
 }
