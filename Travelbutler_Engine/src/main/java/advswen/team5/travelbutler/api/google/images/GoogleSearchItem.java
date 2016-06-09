6410dc917fa463a4535faea18de27d1583bb19ed
at-153328 <at-153328@AP086105.hrw.hs-weingarten.de>
2016-06-09 17:10:20 +0200

    Fix #3

--- Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/api/google/images/GoogleSearchItem.java
+++ Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/api/google/images/GoogleSearchItem.java
@@ -3,12 +3,14 @@ package advswen.team5.travelbutler.api.google.images;
 public class GoogleSearchItem {
 	
 	private String title;
+	private String link;
 	private GoogleSearchImage image;
 	private String mime;
 	
-	public GoogleSearchItem(String title, GoogleSearchImage image, String mime) {
+	public GoogleSearchItem(String title, String link, GoogleSearchImage image, String mime) {
 		super();
 		this.title = title;
+		this.link = link;
 		this.image = image;
 		this.mime = mime;
 	}
@@ -36,6 +38,16 @@ public class GoogleSearchItem {
 	public void setMime(String mime) {
 		this.mime = mime;
 	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+	
 	
 	
 	
