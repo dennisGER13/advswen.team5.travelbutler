b0acc8215715dfde9b0885dfa00a9a1a50a30d95
Emine Öztürk <eo-153416@hs-weingarten>
2016-06-09 12:57:58 +0200

    PDFGenerator test (dummy) erstellt

---
+++ Travelbutler_Engine/src/test/java/advswen/team5/travelbutler/output/PDFGeneratorTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package advswen.team5.travelbutler.output;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Emine & Bünyamin
+ *
+ */
+public class PDFGeneratorTest {
+
+	/**
+	 * Test method for {@link advswen.team5.travelbutler.output.PDFGenerator#PDFGenerator(advswen.team5.travelbutler.api.response.Response)}.
+	 */
+	@Test
+	public void testPDFGenerator() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link advswen.team5.travelbutler.output.PDFGenerator#generate()}.
+	 */
+	@Test
+	public void testGenerate() {
+		fail("Not yet implemented");
+	}
+
+}
