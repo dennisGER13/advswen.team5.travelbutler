196fb358733806e8585225cab5cda0f15b4a17da
Michael Aulbach <ma-152478@hs-weingarten.de>
2016-06-13 11:39:13 +0200

    PDFGeneratorTest put in comments (tests haven't been implemented yet)

--- Travelbutler_Engine/src/test/java/advswen/team5/travelbutler/output/PDFGeneratorTest.java
+++ Travelbutler_Engine/src/test/java/advswen/team5/travelbutler/output/PDFGeneratorTest.java
@@ -1,29 +1,29 @@
-/**
- * 
- */
-package advswen.team5.travelbutler.output;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-
-public class PDFGeneratorTest {
-
-	/**
-	 * Test method for {@link advswen.team5.travelbutler.output.PDFGenerator#PDFGenerator(advswen.team5.travelbutler.api.response.Response)}.
-	 */
-	@Test
-	public void testPDFGenerator() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link advswen.team5.travelbutler.output.PDFGenerator#generate()}.
-	 */
-	@Test
-	public void testGenerate() {
-		fail("Not yet implemented");
-	}
-
-}
+///**
+// * 
+// */
+//package advswen.team5.travelbutler.output;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//
+//
+//public class PDFGeneratorTest {
+//
+//	/**
+//	 * Test method for {@link advswen.team5.travelbutler.output.PDFGenerator#PDFGenerator(advswen.team5.travelbutler.api.response.Response)}.
+//	 */
+//	@Test
+//	public void testPDFGenerator() {
+//		fail("Not yet implemented");
+//	}
+//
+//	/**
+//	 * Test method for {@link advswen.team5.travelbutler.output.PDFGenerator#generate()}.
+//	 */
+//	@Test
+//	public void testGenerate() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
