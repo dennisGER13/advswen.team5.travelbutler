80165c5131d30c6b90400d63f732632f670eae82
dennisGER13 <dw-161380@hs-weingarten.de>
2016-06-13 17:10:05 +0200

    Class ConcreteStrategyCity added (sams as ConcreteStrategyBasic)

---
+++ Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/strategy/ConcreteStrategyCity.java
@@ -0,0 +1,39 @@
+package advswen.team5.travelbutler.strategy;
+
+import advswen.team5.travelbutler.api.APIContainerGoogleGeoCoding;
+import advswen.team5.travelbutler.api.APIContainerGoogleImages;
+import advswen.team5.travelbutler.api.APIContainerTravelbriefing;
+import advswen.team5.travelbutler.api.APIContainerTwitter;
+import advswen.team5.travelbutler.api.APIContainerWikipedia;
+import advswen.team5.travelbutler.api.response.GoogleImagesResponse;
+import advswen.team5.travelbutler.api.response.GoogleMapsResponse;
+
+/*
+ * Author: any author
+ */
+
+import advswen.team5.travelbutler.api.response.Response;
+import advswen.team5.travelbutler.api.response.TravelbriefingResponse;
+import advswen.team5.travelbutler.api.response.TwitterResponse;
+import advswen.team5.travelbutler.api.response.WikipediaResponse;
+
+public class ConcreteStrategyCity implements ISearchStrategy{
+
+	public Response executeSearch(String requestString) {
+	Response response = new Response(requestString);
+	WikipediaResponse wiki = new APIContainerWikipedia().processSearch(response.getDestination());
+	TwitterResponse twitter = new APIContainerTwitter().processSearch(response.getDestination());
+	GoogleMapsResponse googlemaps = new APIContainerGoogleGeoCoding().processSearch(response.getDestination());
+	TravelbriefingResponse travelbriefing = new APIContainerTravelbriefing().processSearch(response.getDestination());
+	GoogleImagesResponse googleImages = new APIContainerGoogleImages().processSearch(response.getDestination());
+	
+	response.setWikipediaResponse(wiki);
+	response.setTwitterResponse(twitter);
+	response.setGooglemapsResponse(googlemaps);
+	response.setTravelbriefingResponse(travelbriefing);
+	response.setGoogleImagesResponse(googleImages);
+		
+	return response;
+	}
+
+}
