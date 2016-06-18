0dc933dbf4dff377771ae49bd165d247ffe4f39c
HSWalohr <al-161385@hs-weingarten.de>
2016-06-18 15:12:08 +0200

    Added Header for enum classes

--- Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/search/GrainEnum.java
+++ Travelbutler_Engine/src/main/java/advswen/team5/travelbutler/search/GrainEnum.java
@@ -1,3 +1,12 @@
+// *************************************************************************************
+// File:         [GrainEnum.java]
+// Created:      [2016/05/24 Tuesday]
+// Last Changed: $Date: 2016/05/24 18:19:00 $
+// Author:       <A HREF="mailto:[al-161385@hs-weingarten.de]">[Alexander Lohr]</A>
+//**************************************************************************************
+//Description: 	Enum für die verschiedenen Grains der Suche
+//				
+//**************************************************************************************
 package advswen.team5.travelbutler.search;
 
 public enum GrainEnum {
